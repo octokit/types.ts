5addbbf178c63c33f5e74bd724986f4446f5ae28
Gregor Martynus <gregor@martynus.net>
2019-11-02 17:11:30 -0700

    feat: AuthInterface & StrategyInterface

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,18 @@
-import { AuthInterface } from "./AuthInterface";
-import { EndpointDefaults } from "./EndpointDefaults";
-import { EndpointInterface } from "./EndpointInterface";
-import { EndpointOptions } from "./EndpointOptions";
-import { Fetch } from "./Fetch";
-import { OctokitResponse } from "./OctokitResponse";
-import { RequestHeaders } from "./RequestHeaders";
-import { RequestInterface } from "./RequestInterface";
-import { RequestMethod } from "./RequestMethod";
-import { RequestOptions } from "./RequestOptions";
-import { RequestParameters } from "./RequestParameters";
-import { RequestRequestOptions } from "./RequestRequestOptions";
-import { ResponseHeaders } from "./ResponseHeaders";
-import { Route } from "./Route";
-import { Signal } from "./Signal";
-import { Url } from "./Url";
+export { AuthInterface } from "./AuthInterface";
+export { EndpointDefaults } from "./EndpointDefaults";
+export { EndpointInterface } from "./EndpointInterface";
+export { EndpointOptions } from "./EndpointOptions";
+export { Fetch } from "./Fetch";
+export { OctokitResponse } from "./OctokitResponse";
+export { RequestHeaders } from "./RequestHeaders";
+export { RequestInterface } from "./RequestInterface";
+export { RequestMethod } from "./RequestMethod";
+export { RequestOptions } from "./RequestOptions";
+export { RequestParameters } from "./RequestParameters";
+export { RequestRequestOptions } from "./RequestRequestOptions";
+export { ResponseHeaders } from "./ResponseHeaders";
+export { Route } from "./Route";
+export { Signal } from "./Signal";
+export { StrategyInterface } from "./StrategyInterface";
+export { Url } from "./Url";
 export { VERSION } from "./VERSION";
-
-export {
-  AuthInterface,
-  EndpointDefaults,
-  EndpointInterface,
-  EndpointOptions,
-  Fetch,
-  OctokitResponse,
-  RequestHeaders,
-  RequestInterface,
-  RequestMethod,
-  RequestOptions,
-  RequestParameters,
-  RequestRequestOptions,
-  ResponseHeaders,
-  Route,
-  Signal,
-  Url
-};
