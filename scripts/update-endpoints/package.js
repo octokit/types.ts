d82fb1c867d9f359e415e51d7e613fa4430eb399
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-11-30 14:47:11 -0800

    build(update-endpoints): update package.json

---
+++ scripts/update-endpoints/package.js
@@ -0,0 +1,19 @@
+const { writeFileSync } = require("fs");
+const prettier = require("prettier");
+
+if (!process.env.VERSION) {
+  throw new Error(`VERSION environment variable must be set`);
+}
+
+const pkg = require("../../package.json");
+
+if (!pkg.octokit) {
+  pkg.octokit = {};
+}
+
+pkg.octokit["openapi-version"] = process.env.VERSION;
+
+writeFileSync(
+  "package.json",
+  prettier.format(JSON.stringify(pkg), { parser: "json" })
+);
