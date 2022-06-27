6170abaeb94df0f8df341dd89bf3952d2fa22ba4
Aleksey Levenstein <levenleven@github.com>
2022-06-27 08:23:58 +0300

    fix(RequestInterface): restore catch all overload

--- src/RequestInterface.ts
+++ src/RequestInterface.ts
@@ -34,7 +34,14 @@ export interface RequestInterface<D extends object = object> {
    * @param {string} route Request method + URL. Example: `'GET /orgs/{org}'`
    * @param {object} [parameters] URL, query or body parameters, as well as `headers`, `mediaType.{format|previews}`, `request`, or `baseUrl`.
    */
-  (route: Route, options?: RequestParameters): Promise<OctokitResponse<any>>;
+  <R extends Route>(
+    route: keyof Endpoints | R,
+    options?: R extends keyof Endpoints
+      ? Endpoints[R]["parameters"] & RequestParameters
+      : RequestParameters
+  ): R extends keyof Endpoints
+    ? Promise<Endpoints[R]["response"]>
+    : Promise<OctokitResponse<any>>;
 
   /**
    * Returns a new `request` with updated route and parameters
