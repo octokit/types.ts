11d6d8ff2469c90f9ad9505374009c929746c8da
Kenichi Kamiya <kachick1@gmail.com>
2022-06-29 04:11:47 +0900

    fix(RequestInterface): Revert method overloading with adding some tests (#405)

--- test.ts
+++ test.ts
@@ -1,9 +1,14 @@
 // This code is not executed, only statically analyzed using `tsc --noEmit`
-import { EndpointInterface, Endpoints } from "./src";
+import { EndpointInterface, Endpoints, RequestInterface, RequestParameters, Route } from "./src";
 
-const endpoint = null as EndpointInterface;
+const endpoint = {} as EndpointInterface;
 function assertString(type: string) {}
+function assertNullableString(type: string | null | undefined) {}
 function assertArray(type: unknown[]) {}
+const assertPaginate = {} as {
+  <R extends Route>(route: R): Promise<void>;
+  <R extends RequestInterface>(request: R): Promise<void>;
+};
 
 const createIssueOptions = {
   owner: "octocat",
@@ -26,13 +31,28 @@ const resultMerge2 = endpoint.merge(createIssueOptions);
 assertString(result.headers["x-foo"]);
 assertString(resultMerge.title);
 assertString(resultMerge.headers["x-foo"]);
-assertString(resultMerge2.url);
+assertNullableString(resultMerge2.url);
 
 const test = {} as Endpoints["GET /repos/{owner}/{repo}/issues"]["response"];
 assertArray(test.data);
-assertString(test.data[0].assignees[0].avatar_url);
+assertString(test.data[0].assignees![0].avatar_url);
 const test2 = {} as Endpoints["GET /scim/v2/organizations/{org}/Users"]["response"];
-assertString(test2.data.Resources[0].name.givenName);
+assertNullableString(test2.data.Resources[0].name.givenName);
 
 const test3 = {} as Endpoints["POST /user/repos"]["parameters"];
 assertString(test3.name);
+
+const checkRunsRoute = 'GET /repos/{owner}/{repo}/commits/{ref}/check-runs' as const;
+
+assertPaginate(checkRunsRoute);
+
+const listForRef = {} as {
+  (params?: RequestParameters & Omit<Endpoints["GET /repos/{owner}/{repo}/commits/{ref}/check-runs"]["parameters"], "baseUrl" | "headers" | "mediaType">): Promise<Endpoints[typeof checkRunsRoute]["response"]>;
+  defaults: RequestInterface["defaults"];
+  endpoint: EndpointInterface<{
+      url: string;
+  }>;
+};
+
+// octokit.paginate can take `request` method. ref: https://github.com/octokit/plugin-paginate-rest.js/blob/b3fb11e301f9658554e110aeebbd7cbb89b8aad4/README.md?plain=1#L117-L126
+assertPaginate(listForRef);
