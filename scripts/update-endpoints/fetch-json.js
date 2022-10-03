3fcfd122a9df225cead3027b89f49558cae399f4
Tim Rogers <me@timrogers.co.uk>
2022-10-03 11:46:04 +0100

    fix(build): don't show Enterprise Cloud in the documentation URLs now we're using the Enterprise Cloud schema

--- scripts/update-endpoints/fetch-json.js
+++ scripts/update-endpoints/fetch-json.js
@@ -33,6 +33,18 @@ const QUERY = `
 
 main();
 
+// In order to see operations which are only available in GitHub Enterprise Cloud
+// (GHEC), `github-openapi-graphql-query` now looks at the GHEC-specific OpenAPI
+// specifications. At the moment, the documentation URLs, therefore, are not
+// normalised. This removes the references to GHEC.
+const removeEnterpriseCloudFromDocumentationUrl = (endpoint) => ({
+  ...endpoint,
+  documentationUrl: endpoint.documentationUrl.replace(
+    "/enterprise-cloud@latest/",
+    ""
+  ),
+});
+
 async function main() {
   const {
     data: { endpoints },
@@ -43,8 +55,11 @@ async function main() {
 
   writeFileSync(
     path.resolve(__dirname, "generated", "endpoints.json"),
-    prettier.format(JSON.stringify(endpoints), {
-      parser: "json",
-    })
+    prettier.format(
+      JSON.stringify(endpoints.map(removeEnterpriseCloudFromDocumentationUrl)),
+      {
+        parser: "json",
+      }
+    )
   );
 }
