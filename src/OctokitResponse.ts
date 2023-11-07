7269dcc2040810645a062f84f9775da701f9fded
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2023-11-07 12:46:37 -0500

    feat(types): make `OctokitResponse` an interface (#594)
    * fix(types): make `OctokitResponse` an interface
    
    * style: prettier

--- src/OctokitResponse.ts
+++ src/OctokitResponse.ts
@@ -1,7 +1,7 @@
 import type { ResponseHeaders } from "./ResponseHeaders";
 import type { Url } from "./Url";
 
-export type OctokitResponse<T, S extends number = number> = {
+export interface OctokitResponse<T, S extends number = number> {
   headers: ResponseHeaders;
   /**
    * http response code
@@ -15,4 +15,4 @@ export type OctokitResponse<T, S extends number = number> = {
    * Response data as documented in the REST API reference documentation at https://docs.github.com/rest/reference
    */
   data: T;
-};
+}
