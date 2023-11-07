afb973534209a497bee1fa689b7a8cc66d02afdd
wolfy1339 <webmaster@wolfy1339.com>
2023-11-06 21:47:39 -0500

    style: prettier

--- src/OctokitResponse.ts
+++ src/OctokitResponse.ts
@@ -15,4 +15,4 @@ export interface OctokitResponse<T, S extends number = number> {
    * Response data as documented in the REST API reference documentation at https://docs.github.com/rest/reference
    */
   data: T;
-};
+}
